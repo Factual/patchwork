f91e7462dce3f3d568968505ac224c749348e970
Laura Eckman <laura@factual.com>
2017-08-02 10:54:50 -0700

    remove all non-standard libraries

--- src/helpers.py
+++ src/helpers.py
@@ -1,5 +1,5 @@
-from datetime import datetime, timedelta
-import time, pytz, tzlocal
+from datetime import datetime, timedelta, timezone
+import time
 
 def convert_versioneye_timestamp(time_string):
     date_template = "%Y-%m-%dT%H:%M:%S"
@@ -7,9 +7,9 @@ def convert_versioneye_timestamp(time_string):
     if not 'T' in time_string:
         date_template = date_template.split('T')[0]
 
-    local_timezone = tzlocal.get_localzone()
+    local_timezone = datetime.now(timezone.utc).astimezone().tzinfo
     utc_time = datetime.strptime(time_string, date_template)
-    local_time = utc_time.replace(tzinfo=pytz.utc).astimezone(local_timezone)
+    local_time = utc_time.replace(tzinfo=timezone.utc).astimezone(local_timezone)
     return time.mktime(local_time.timetuple())
 
 def get_display_name(fname, params):
